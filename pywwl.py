989afc19d4ec34fea201be553ffafdf4c1b9a32f
fabiux <battormoros@gmail.com>
2016-02-18 09:55:46 +0100

    PEP8

--- pywwl.py
+++ pywwl.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
-
-'''
+"""
 pywwl - Compute distance and azimuth between two Maidenhead squares.
 
 This program is derived from wwl by Diane Bruce (VA3DB).
@@ -16,39 +15,39 @@ According to original software, this program is distributed under a BSD License.
 @license: BSD
 @author: Fabio Pani (IZ2UQF)
 @version: 0.2_beta
-'''
-
-__version__ = "0.2_beta"
-
+"""
 import sys
 import wwl
 
+__version__ = "0.2_beta"
+
+
 def main():
-	if len(sys.argv) < 2:
-		print "Usage: pywwl home_locator [dx_locator]"
-		return False
+    if len(sys.argv) < 2:
+        print "Usage: pywwl home_locator [dx_locator]"
+        return False
 
-	my_wwl = sys.argv[1].upper()
-	if not wwl.isValidLocator(my_wwl):
-		print sys.argv[1], "is not a valid locator"
-		return False
+    my_wwl = sys.argv[1].upper()
+    if not wwl.is_valid_locator(my_wwl):
+        print sys.argv[1], "is not a valid locator"
+        return False
 
-	my_location = wwl.convertLocator(my_wwl)
+    my_location = wwl.convert_locator(my_wwl)
 
-	if len(sys.argv) == 2:
-		print "Locator    :", sys.argv[1]
-		print "Coordinates: Long:", wwl.getLongitude(my_location), "- Lat:", wwl.getLatitude(my_location)
-		return True
+    if len(sys.argv) == 2:
+        print "Locator    :", sys.argv[1]
+        print "Coordinates: Long:", wwl.get_longitude(my_location), "- Lat:", wwl.get_latitude(my_location)
+        return True
 
-	dx_wwl = sys.argv[2].upper()
-	dx_location = wwl.convertLocator(dx_wwl)
-	if not wwl.isValidLocator(dx_wwl):
-		print sys.argv[2], "is not a valid locator"
-		return False
+    dx_wwl = sys.argv[2].upper()
+    dx_location = wwl.convert_locator(dx_wwl)
+    if not wwl.is_valid_locator(dx_wwl):
+        print sys.argv[2], "is not a valid locator"
+        return False
 
-	wwl.bearingDistance(my_location, dx_location)
-	print "qrb:", int(round(wwl.distance, 0)), "kilometers, azimuth:", int(round(wwl.bearing, 0)), "degrees"
-	return True
+    wwl.bearing_distance(my_location, dx_location)
+    print "qrb:", int(round(wwl.distance, 0)), "kilometers, azimuth:", int(round(wwl.bearing, 0)), "degrees"
+    return True
 
 if __name__ == '__main__':
-	main()
+    main()
